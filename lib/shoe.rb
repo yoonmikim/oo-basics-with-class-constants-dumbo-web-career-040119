87d11bf3469734c43ca7eea427f37dc70687f439
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-04-04 02:33:14 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -13,11 +13,4 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
-  def brand=(brand)
-    @brand = brand
-    if !BRANDS.include?(@brand)
-      BRANDS << @brand
-    end
-  end
-
 end
